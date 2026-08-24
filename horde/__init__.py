37677fc373f2e3732966f2bfe9dab1b00552a87c
mainstreetmediatn-git <mainstreetmediatn@gmail.com>
2026-08-24 12:11:19 -0500

    Export operator policy domain types

--- horde/__init__.py
+++ horde/__init__.py
@@ -1,13 +1,27 @@
 """Linux Recon Horde core package."""
 
-from .models import Agent, AgentState, Evidence, MemoryRecord, MissionContract
+from .models import (
+    Agent,
+    AgentState,
+    EnforcementMode,
+    Evidence,
+    ExecutionRequest,
+    MemoryRecord,
+    MissionContract,
+    OperatorPolicy,
+    RiskLevel,
+)
 from .lifecycle import LifecycleManager
 
 __all__ = [
     "Agent",
     "AgentState",
+    "EnforcementMode",
     "Evidence",
+    "ExecutionRequest",
     "MemoryRecord",
     "MissionContract",
+    "OperatorPolicy",
+    "RiskLevel",
     "LifecycleManager",
 ]
